c473dc4a5a34ef33af4b028b29cb88c92e80d902
isacetin <isacetinn@outlook.com>
2021-03-15 18:58:33 +0300

    is modeli olusturuldu

---
+++ lib/model/is_ilan.dart
@@ -0,0 +1,9 @@
+class IsIlan{
+  String isAdi;
+  String isDetay;
+  String isUcret;
+  String isZaman;
+  String isAdres;
+
+  IsIlan({this.isAdi, this.isDetay, this.isUcret, this.isZaman, this.isAdres});
+}
